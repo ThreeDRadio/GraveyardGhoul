8a49069bdc53d01edca26952b4447627c1462cd9
Michael Marner <michael.marner@unisa.edu.au>
2014-06-23 08:20:12 +0930

    Interface to the music catalogue

---
+++ MusicLibrary.py
@@ -0,0 +1,24 @@
+
+
+
+class MusicLibrary:
+
+    def __init__(self, configFile):
+        print "Connecting to Music Library"
+
+
+    def getRandomDemo():
+        print "Getting random Demo"
+
+    def getRandomLocal():
+        print "Getting random Local"
+
+    def getRandomAustralian(): 
+        print "Getting random Australian"
+
+    def getRandomSong(female): 
+        if female == true:
+            print "Finding song featuring female artist"
+        else:
+            print "finding random song"
+
