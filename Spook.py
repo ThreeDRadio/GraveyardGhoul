a6070ac53a78c524033f46ed004ad4757a9b661b
Michael Marner <michael.marner@unisa.edu.au>
2014-06-23 08:20:00 +0930

    Loading config file.

--- Spook.py
+++ Spook.py
@@ -1,14 +1,21 @@
 #!./env/bin/python
-import pygtk
-pygtk.require('2.0')
-import gtk
+import yaml
 from SpookGUI import SpookGUI 
+from MusicLibrary import MusicLibrary
 
 print "Spook - The Three D Radio Graveyard Manager"
 print "Copyright 2014 Michael Marner <michael@20papercups.net>"
 
-gui = SpookGUI()
-gui.main()
+print "Loading config file... "
+configStream = file('config.yaml', 'r')
+config = yaml.load(configStream)
+yaml.dump(config)
+
+
+
+#gui = SpookGUI()
+#gui.main()
+
 
 print "GUI Loaded"
 
